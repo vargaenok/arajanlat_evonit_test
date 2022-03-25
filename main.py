e7a7a7d3fc8c0c93eea7366b84694a82d94945a3
vargaenok <egonsi007@gmail.com>
2022-03-26 00:49:52 +0100

    FIX input field profit_percent and usd_huf_change changed into float from string

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def input_handling():
     start = input("Starting Port: ")
     finish = input("Target City: ")
     container_type = input("Container type (20ST, 40ST, 40HC): ")
-    profit_percent = input("Profit margin: ")
-    usd_huf_change = input("USD/HUF exchange rate: ")
+    profit_percent = float(input("Profit margin: "))
+    usd_huf_change = float(input("USD/HUF exchange rate: "))
     print(start, finish, container_type, profit_percent, usd_huf_change)
 
 
